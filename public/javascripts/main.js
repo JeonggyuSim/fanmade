5f5e768d6de3fc693133ac9f07246fbc7df83bb6
JeonggyuSim <wjdrb2035@naver.com>
2022-01-07 19:53:14 +0900

    Feat: 뒤로가기, modal 열기 추가

---
+++ public/javascripts/main.js
@@ -0,0 +1,28 @@
+const backBtn = document.querySelectorAll('.back-btn');
+const modal = document.querySelector('.modal');
+
+// 뒤로가기
+if (backBtn) {
+  backBtn.forEach((element) => {
+    element.addEventListener('click', () => {
+      if (document.referrer) window.history.back();
+      else location.href = "./index.html";
+    });
+  });
+}
+
+// modal 열기
+if (modal) {
+  const body = document.body;
+  const modalBackground = document.querySelector('.modal__background');
+  const modalBtn = document.querySelector('.detail-bar__button-wrapper .btn:first-child');
+
+  function modalCloseHandler() {
+    body.classList.toggle('overflow--hidden')
+    modal.classList.toggle('hidden')
+  }
+
+  modalBtn.addEventListener('click', modalCloseHandler);
+
+  modalBackground.addEventListener('click', modalCloseHandler);
+}
